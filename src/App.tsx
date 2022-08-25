c885ea520a527532a6e4e0875aaf215451f97029
plasmarad <userofgogle356@gmail.com>
2022-08-25 14:24:06 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import {Helmet, HelmetProvider} from 'react-helmet-async';
+
+import * as MUI from '@mui/material/'
 import Button from '@mui/material/Button';
 
-function App() {
-  return (
-    <div className = "App">
-      	<HelmetProvider>
-        	<Helmet>
-          		<title>My website</title>
-        	</Helmet>
+//import theme
 
-			<Button variant="contained" color="primary">hi</Button>
-      	</HelmetProvider>
+import {DefaultTheme} from './theme';
+
+function head() {
+  return (
+	<HelmetProvider>
+		<Helmet>
+		  <title>plasmarad.me</title>
+		  <meta name="description" content="My description" />
+		</Helmet>
+	</HelmetProvider>
+  );
+}
+
+export default function App() {
+  return (
+    <div className = "App">	
+      	{ head() }
+		<MUI.ThemeProvider theme={DefaultTheme}>
+			<MUI.Divider></MUI.Divider>
+		</MUI.ThemeProvider>
     </div>
   );
 }
 
-export default App;
