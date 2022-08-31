95b0fee871bd54038f39dc5d8d2c12f34a862406
plasmarad <userofgogle356@gmail.com>
2022-08-31 02:12:55 -0400

    Progress todo: slide from container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Helmet, HelmetProvider} from 'react-helmet-async';
-import Button from '@mui/material/Button';
 
-//import theme
-import {DefaultTheme} from './assets/theme';
+
+import { AppBar, Box, Button, Container, IconButton, ThemeProvider, Toolbar, Typography, Slide, ButtonGroup } from '@mui/material';
 
 //import svg
 import {ConfCog_svg} from './assets/svg_cog521x512';
+import {DefaultTheme} from './assets/theme';
 
 function HeadMetadata(props:any) {
   return (
@@ -19,13 +19,54 @@ function HeadMetadata(props:any) {
   );
 }
 
+function AnimateAppBar(props:any) {
+	return (
+		<Slide direction="right" in={props.MenuOpen} mountOnEnter unmountOnExit>
+			<ButtonGroup variant="text" color="secondary" aria-label="Configuration slide in button group">
+				<Button>One</Button>
+				<Button>Two</Button>
+				<Button>Three</Button>
+			</ButtonGroup>
+		</Slide>
+	)
+
+}
+
+function HeadBar(props: any){
+	{/* set a state for when displaying menu options */}
+	const [menuOpen, setMenuOpen] = React.useState(false);
+	
+	const handleChange = () => {
+		setMenuOpen((prev) => !prev);
+	};
+
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+      		<AppBar position="static">
+				<Container maxWidth="lg">
+      		  	<Toolbar>
+				
+					<IconButton edge="start" color="inherit" aria-label="menu" onClick={handleChange}>
+						<ConfCog_svg />
+					</IconButton>
+					<AnimateAppBar MenuOpen={menuOpen}  />
+
+
+			  	</Toolbar>
+			</Container>
+      		</AppBar>
+    	</Box>
+	)
+}
 
 export default function App() {
   return (
     <div className = "App">	
       	<HeadMetadata />
 
-		{/* Add an App bar and research containers/components */}
+		<ThemeProvider theme={DefaultTheme}>
+			<HeadBar />
+		</ThemeProvider>
 
     </div>
   );
--- src/assets/theme.tsx
+++ src/assets/theme.tsx
@@ -11,7 +11,7 @@ declare module '@mui/material/styles' {
         danger?: string;
       };
     }
-  }
+}
   
 
 
