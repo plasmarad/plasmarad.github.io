a888ee8a61d7f2cb7ea33e8547367cef4edb42c1
plasmarad <userofgogle356@gmail.com>
2022-09-10 01:11:56 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Helmet, HelmetProvider} from 'react-helmet-async';
 
 
 import { AppBar, Box, Button, IconButton, ThemeProvider, Toolbar, Typography, Slide, ButtonGroup, Fade, DialogContent, Dialog, DialogActions, DialogContentText, DialogTitle, Alert } from '@mui/material';
@@ -18,6 +17,7 @@ function HeadBar(props: any){
 	};
 
 	return (
+		
 		<Box sx={{
 			overflow: 'hidden',
 		}} ref={containerRef} >
@@ -36,7 +36,7 @@ function HeadBar(props: any){
 						<div>
 						<Fade in={menuOpen} timeout={500} mountOnEnter unmountOnExit>
 						<ButtonGroup variant="contained" color="secondary" aria-label="Configuration slide in button group">
-							<Button>Change color theme </Button>
+							<Button>color theme </Button>
 							<Button>Behavior</Button>
 							<Button>Website info</Button>
 						</ButtonGroup>
@@ -50,25 +50,16 @@ function HeadBar(props: any){
 }
 
 export default function App() {
-	const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-  return (
-	 <div className = "App">	
-
+	return (
+	 <div className = "App">
 
 		{/* TODO: when page loads, display alert warning site is WIP */}
 		<ThemeProvider theme={DefaultTheme}>
-		
 			<HeadBar />
-
+			<Box sx={{m:1}}></Box>
 			<Alert severity="warning">This is website is under development. Some functions might not work as expected.</Alert>
+			
+			{/* Make a simple blog using a react component. Keep it simply stupid*/}
 
 		</ThemeProvider>
 
