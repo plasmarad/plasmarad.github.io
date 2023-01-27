4f76f1cb693626a321b7b0fc57f226165b01398e
plasmarad <userofgogle356@gmail.com>
2023-01-27 00:21:54 -0500

    tst

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation on sveltekit</p>
 <p>I change one thing and it stops working</p>
+
+<!-- display an image -->
+<img src="https://github.com/plasmarad/plasmarad.github.io/blob/4eec996507f3b3b45fb7f578e0cfaf47828ad079/static/favicon.png" alt="svelte" width="500" height="600">
