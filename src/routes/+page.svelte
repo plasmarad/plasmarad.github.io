ca0657a6f24cb43e545a8def2b0f0250040347ff
plasmarad <userofgogle356@gmail.com>
2023-01-26 22:39:57 -0500

    updated home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
+<title>plasmarad.ME</title>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation on svelte</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation on sveltekit</p>
