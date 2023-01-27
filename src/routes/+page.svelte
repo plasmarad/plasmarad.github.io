2e17945609f7581b3a7e0aa0072a81cbae607426
plasmarad <userofgogle356@gmail.com>
2023-01-27 16:51:43 -0500

    pls work now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation on sveltekit</p>
 <p>I change one thing and it stops working</p>
 
+<p>please work now...</p>
 
-<p> there is an issue with this page where it yields a 404 on firefox. </p>
+<!-- <p> there is an issue with this page where it yields a 404 on firefox. </p> -->
 <!-- most likely issue is that gh-pages is not set up properly -->
