327711601ec8fbf636a900a294850256fb98f675
plasmarad <userofgogle356@gmail.com>
2023-02-10 20:40:24 -0500

    prototype_v.1

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<nav class = "navbar">
+    <a href="/">Home</a>
+</nav>
+
+<div class = "page">
+    <slot />
+</div>
+<style>
+    .navbar {
+        background-image: linear-gradient(to right, purple, orange);
+        overflow: hidden;
+        position: fixed;
+        top: 0; right: 0; 
+        width: 100%;        
+    }
+     .navbar a {
+        float: left;
+        color: white;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 20px;
+
+    }
+
+    .navbar a:hover {
+        color: gray;
+    }
+
+    .page {
+        padding-top: 15px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <title>plasmarad.ME</title>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation on sveltekit</p>
-<p>I change one thing and it stops working</p>
+<script>
+    import Layout from './+layout.svelte';
+</script>
 
-<p>please work now...</p>
-
-<!-- <p> there is an issue with this page where it yields a 404 on firefox. </p> -->
-<!-- most likely issue is that gh-pages is not set up properly -->
+<!-- <Layout>
+    <h1>plasmarad.ME</h1>
+    <p>
+        Welcome to my site! wo
+    </p>
+</Layout> -->
+<h1> wow</h1>
