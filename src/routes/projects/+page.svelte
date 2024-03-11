6884ff1d0c0a05f334c8c9039dabdfa810291df9
plasmarad <73721329+plasmarad@users.noreply.github.com>
2024-03-11 15:28:48 +0000

    cleaned up and updated project's list.

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -8,47 +8,55 @@
 <script>
     import Card from './card.svelte';
 
-    import polaris_cardico from '$lib/images/polaris_card.png';
+    // import polaris_cardico  from '$lib/images/polaris_card.png';
     import alkaline_cardico from '$lib/images/alkaline_card_2.png';
-	import durango_cardico from '$lib/images/durango_banner.png';
-    import website_cardico from '$lib/images/website_card.png';
+	import durango_cardico  from '$lib/images/durango_banner.png';
+    import website_cardico  from '$lib/images/website_card.png';
 
-    const cpp_chip      = {color: 'purple'    ,  text: 'C++'     };
-    const durango_chip  = {color: 'orange'    ,  text: 'Durango' };
-    const python_chip   = {color: 'cyan'      ,  text: 'Python'  };
-    const rust_chip     = {color: 'teal'      ,  text: 'Rust'    };
-    const cli_chip      = {color: 'lightgray' ,  text: 'CLI'     };
-    const svelte_chip   = {color: 'red'       ,  text: 'Svelte'  };
-    const js_chip       = {color: 'yellow'    ,  text: 'WebDev'      };
-    const studio_chip   = {color: 'lightgreen',  text: 'Studio'  };
-    const FOSS_chip     = {color: 'white'     ,  text: 'FOSS'    };
+    const cpp_chip      = {color: 'blue'        ,  text: 'C++'        };
+    const Cs_chip       = {color: 'purple'      ,  text: 'C#'         };
+    const Avalonia_chip = {color: 'magenta'     ,  text: 'Avalonia'   };
+    const durango_chip  = {color: 'orange'      ,  text: 'Durango'    };
+    const studio_chip   = {color: 'lightgreen'  ,  text: 'Studio'     };
+    const Game_chip     = {color: 'red'         ,  text: 'Game'       };
+    const svelte_chip   = {color: 'red'         ,  text: 'Svelte'     };
+    const js_chip       = {color: 'yellow'      ,  text: 'WebDev'     };
 
 </script>
 	
 
 
 <div class="PanelGrid">
-
+    <!--
     <Card banner={polaris_cardico} 
     chips={[cpp_chip, python_chip, durango_chip, FOSS_chip]}
     desc="3D engine designed for the Quasar Framework" 
-    />
+    /> -->
 
     <Card banner={durango_cardico}
-    desc='Custom in house production programming language' 
-    chips={[cpp_chip, durango_chip, FOSS_chip]}
-    banner_inject_style=";background-color: #000000;background-size: 150px "
+    desc='programming language fusing C++ and Python' 
+    chips={[cpp_chip, durango_chip]}
+    banner_inject_style="background-color: #000000;background-size: 150px " 
     />
-
 
     <Card banner={website_cardico}
     desc='my portfolio website made with svelte-kit' 
     chips={[svelte_chip, js_chip]}
     banner_inject_style="background-color: #000000;"
     />
+    
+    <Card
+    desc='motion graphics animator'
+    chips={[Cs_chip, Avalonia_chip]}
+    />
+
+    <Card
+    desc='movement focused supersoldier warfare FPS game.'
+    chips={[Game_chip]}
+    />
 
     <Card banner={alkaline_cardico}
-    desc='Entertainment & technologies studio'
+    desc='Entertainment & technology studio'
     chips={[studio_chip]}
     banner_inject_style="background-color: #000000;"
     />
